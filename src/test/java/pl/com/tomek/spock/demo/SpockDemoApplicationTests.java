00f69ec5d50fbccc56e5738e87e1d07b7f3a9ca4
tomekmazur95 <tomek.mazur95@gmail.com>
2023-01-07 21:13:15 +0100

    added spring boot project

---
+++ src/test/java/pl/com/tomek/spock/demo/SpockDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.tomek.spock.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpockDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
