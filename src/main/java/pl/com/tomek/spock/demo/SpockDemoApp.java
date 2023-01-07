6754fdaca14dfb5256c5e8b67ecabae9390d5616
tomekmazur95 <tomek.mazur95@gmail.com>
2023-01-07 21:19:16 +0100

    rename main class

--- src/main/java/pl/com/tomek/spock/demo/SpockDemoApp.java
+++ src/main/java/pl/com/tomek/spock/demo/SpockDemoApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpockDemoApplication {
+public class SpockDemoApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpockDemoApplication.class, args);
+		SpringApplication.run(SpockDemoApp.class, args);
 	}
 
 }
--- src/test/java/pl/com/tomek/spock/demo/SpockDemoAppTests.java
+++ src/test/java/pl/com/tomek/spock/demo/SpockDemoAppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpockDemoApplicationTests {
+class SpockDemoAppTests {
 
 	@Test
 	void contextLoads() {
